f0dcfb202108ec72a54018a2ac68ff3ddbf0dc41
bkoplin <benjaminkoplin@gmail.com>
2021-02-27 08:46:44 -0600

    Update urlsAndUrns.js
    Make file of all urns and urls

--- urlsAndUrns.js
+++ urlsAndUrns.js
@@ -16,14 +16,14 @@
 import packageJson from "./package.json";
 
 const {
-  appInfo: { loki },
+  appInfo,
 } = packageJson;
-export const baseUrl = `https://${loki.cloudPrefix}.saplingdata.com/${loki.appCodeName}-AppBuilder/api`;
+export const baseUrl = `https://${appInfo.loki.cloudPrefix}.saplingdata.com/${appInfo.loki.appCodeName}-AppBuilder/api`;
 export const resourceUrl = "/urn/com/loki/core/model/api/resource/v";
-export const pageFileListUrl = `/urn/com/loki/core/model/api/list/v/urn/com/${loki.cloudCodeName}/${loki.appCodeName}/app/pages/${loki.pageCodeName}?format=json`;
-export const pageFileUploadUrl = `/urn/com/loki/core/model/api/resource/v/urn/com/${loki.cloudCodeName}/${loki.appCodeName}/app/pages/${loki.pageCodeName}!`;
-export const pageDataUploadUrl = `/urn/com/loki/modeler/model/types/combinedPageExt/v/urn/com/${loki.cloudPrefix}/${loki.appCodeName}/app/pages/${loki.pageCodeName}`;
-export const queryUploadUrl = `/urn/com/loki/modeler/model/types/queryExt/v/urn/com/${loki.cloudPrefix}/${loki.appCodeName}/model/queries/${loki.pageCodeName}`;
-export const queryBaseUrn = `urn:com:${loki.cloudPrefix}:${loki.appCodeName}:model:queries:${loki.pageCodeName}`;
+export const pageFileListUrl = `/urn/com/loki/core/model/api/list/v/urn/com/${appInfo.loki.cloudCodeName}/${appInfo.loki.appCodeName}/app/pages/$appInfo.{loki.pageCodeName}?format=json`;
+export const pageFileUploadUrl = `/urn/com/loki/core/model/api/resource/v/urn/com/${appInfo.loki.cloudCodeName}/${appInfo.loki.appCodeName}/app/pages/$appInfo.{loki.pageCodeName}!`;
+export const pageDataUploadUrl = `/urn/com/loki/modeler/model/types/combinedPageExt/v/urn/com/${appInfo.loki.cloudPrefix}/${appInfo.loki.appCodeName}/app/pages/${appInfo.loki.pageCodeName}`;
+export const queryUploadUrl = `/urn/com/loki/modeler/model/types/queryExt/v/urn/com/${appInfo.loki.cloudPrefix}/${appInfo.loki.appCodeName}/model/queries/${appInfo.loki.pageCodeName}`;
+export const queryBaseUrn = `urn:com:${appInfo.loki.cloudPrefix}:${appInfo.loki.appCodeName}:model:queries:${appInfo.loki.pageCodeName}`;
 
-export const queryApiUrl = `/${loki.appCodeName}/api/urn/com/loki/core/model/api/query/v`;
+export const queryApiUrl = `urn/com/loki/core/model/api/query/v/`;
