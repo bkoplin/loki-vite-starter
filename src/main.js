62e025fa28a9a7c300d2d4cd7c43230723a1a7ea
bkoplin <benjaminkoplin@gmail.com>
2021-02-27 06:48:44 -0600

    Update main.js
    Remove explicit imports for Vue components; rely on vite-plugin-components to import

--- src/main.js
+++ src/main.js
@@ -1,20 +1,9 @@
-import "primeicons/primeicons.css";
-import "primevue/resources/primevue.min.css";
-import "primevue/resources/themes/bootstrap4-light-blue/theme.css";
-
-import Column from "primevue/column";
-import PrimeVue from "primevue/config";
-import DataTable from "primevue/datatable";
-import TreeTable from "primevue/treetable";
 import { createApp } from "vue";
 
 import App from "./App.vue";
 
 const app = createApp(App);
 
-app.use(PrimeVue);
+const vm = app.mount('#app');
 
-app.component('DataTable', DataTable);
-app.component('TreeTable', TreeTable);
-app.component('Column', Column);
-app.mount('#app');
+export default { app, vm };
