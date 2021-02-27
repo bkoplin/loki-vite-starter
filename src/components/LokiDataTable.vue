cdae58b735996c5fdd2f9daca353351b7ad72c9d
bkoplin <benjaminkoplin@gmail.com>
2021-02-27 08:40:34 -0600

    Update LokiDataTable.vue
    Use loki object to get api service prefix

--- src/components/LokiDataTable.vue
+++ src/components/LokiDataTable.vue
@@ -1,47 +1,46 @@
 <template>
-  <TreeTable :value="results">
+  <DataTable :value="results" class="p-datatable-sm">
     <Column
       v-for="col of columns"
       :field="col.field"
       :header="col.header"
       :key="col.field"
+      sortable="true"
     ></Column>
-</TreeTable>
+</DataTable>
 
 </template>
 
 <script>
 // @ts-check
+/* eslint-disable no-console */
+import "primeicons/primeicons.css";
+import "primevue/resources/primevue.min.css";
+import "primevue/resources/themes/bootstrap4-light-blue/theme.css";
 import axios from 'axios';
 import { zipObject, upperCase } from "lodash-es";
-import { reactive } from 'vue';
+import { reactive, inject } from 'vue';
+import { queryApiUrl, queryBaseUrn } from '../../urlsAndUrns';
 
 export default {
   setup() {
+    const loki = inject('loki');
+    console.log(loki);
+    let queryUrl = queryApiUrl;
+    if (loki) queryUrl = loki.web.getApiServicePrefix() + queryUrl;
+
     const state = reactive({
       columns: [],
       results: [],
     });
-    if (import.meta.env.DEV) {
-      axios.get('/query', {
-        params: { queryUrn: 'urn:com:reedsmith:cobra:data:insights:Q2EELQGJ:components:component1' },
-        auth: { username: 'bkoplin', password: 'meat shifty hunker 1' },
-      }).then((d) => {
-        console.log(d);
-        const { data: { columnNames, results } } = d;
-        state.columns = columnNames.map((c) => ({ field: c, header: upperCase(c) }));
-        state.results = results.map((r) => zipObject(columnNames, r));
-      });
-    } else {
-      axios.get("https://reedsmith.saplingdata.com/cobra/api/urn/com/loki/core/model/api/query/v/", {
-        params: { queryUrn: 'urn:com:reedsmith:cobra:data:insights:Q2EELQGJ:components:component1' },
-      }).then((d) => {
-        console.log(d);
-        const { data: { columnNames, results } } = d;
-        state.columns = columnNames.map((c) => ({ field: c, header: upperCase(c) }));
-        state.results = results.map((r) => zipObject(columnNames, r));
-      });
-    }
+    axios.get(queryUrl, {
+      params: { queryUrn: `${queryBaseUrn}#test` },
+    }).then((d) => {
+      console.log(d);
+      const { data: { columnNames, results } } = d;
+      state.columns = columnNames.map((c) => ({ field: c, header: upperCase(c) }));
+      state.results = results.map((r) => zipObject(columnNames, r));
+    });
 
     return state;
   },
