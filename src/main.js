04920b49e4e00263994cd317883b53574f92a00f
bkoplin <benjaminkoplin@gmail.com>
2021-03-03 07:20:02 -0600

    Update main.js
    Remove @purge-icons

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import "primevue/resources/primevue.min.css";
 import "primevue/resources/themes/bootstrap4-light-blue/theme.css";
 import { createApp } from "vue";
 import App from "./App.vue";
-import '@purge-icons/generated';
 
 const app = createApp(App);
 const vm = app.mount('#app');
