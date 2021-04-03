c64b314f55e6a296077ddec7223f014467ba36b1
bkoplin <17945159+bkoplin@users.noreply.github.com>
2021-04-03 06:29:19 -0500

    Update main.js
    Import primeflex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
+/* eslint-disable sort-imports */
+
+console.log(import.meta.env)
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import "primeflex/primeflex.css";
+import "primevue/resources/themes/bootstrap4-light-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+
+import PrimeVue from 'primevue/config';
+
 const app = createApp(App);
+app.use(PrimeVue);
 const vm = app.mount('#app');
 
 export default { app, vm };
