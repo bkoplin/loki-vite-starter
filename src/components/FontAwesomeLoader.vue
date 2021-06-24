be15ed017e1dd9d264c7838fab0ce0cdd320e7f5
bkoplin <17945159+bkoplin@users.noreply.github.com>
2021-06-24 05:56:47 -0500

    type properties

--- src/components/FontAwesomeLoader.vue
+++ src/components/FontAwesomeLoader.vue
@@ -5,7 +5,11 @@
     @after-enter="animateIn"
     @leave="animateOut"
   >
-    <div v-show="show" ref="thisRef" class="inset-0 absolute">
+    <div
+      v-show="show"
+      ref="thisRef"
+      class="inset-0 absolute"
+    >
       <div
         class="
           flex flex-col
@@ -17,10 +21,10 @@
         "
       >
         <font-awesome-icon
-          :icon="lightCog"
           ref="iconRef"
+          :icon="lightCog"
           :spin="spin"
-          :size="'3x'"
+          :size="faProps.size"
         ></font-awesome-icon>
       </div>
     </div>
@@ -28,43 +32,44 @@
 </template>
 
 <script lang="ts" setup>
-import Vue, {
-  defineProps,
-  defineEmit,
-  ref,
-  toRefs,
-  toRaw,
-  watch,
-  watchEffect,
-  reactive,
+import * as Vue from 'vue'
+import * as FA from '@fortawesome/fontawesome-svg-core'
+import * as FAV from '@fortawesome/vue-fontawesome'
+import {
   computed,
-} from "vue";
-import * as FA from "@fortawesome/fontawesome-svg-core";
-import * as FAV from "@fortawesome/vue-fontawesome";
-import { gsap } from "gsap";
-import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+  defineEmit,
+  defineProps,
+  reactive,
+  ref,
+  toRaw,
+  toRefs,
+  watch,
+  watchEffect
+} from 'vue'
+import { gsap } from 'gsap'
+import { MorphSVGPlugin } from 'gsap/MorphSVGPlugin'
 
 import {
-  faCog as lightCog,
   faCheckCircle as lightCheckCircle,
-} from "@fortawesome/pro-light-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-gsap.registerPlugin(MorphSVGPlugin);
-type Icon = ReturnType<typeof FA.icon>;
+  faCog as lightCog
+} from '@fortawesome/pro-light-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+gsap.registerPlugin(MorphSVGPlugin)
+type Icon = ReturnType<typeof FA.icon>
 
 type IconAttributes = {
-  d: gsap.SVGPathValue;
-  fill: "currentColor";
-};
+  d: gsap.SVGPathValue
+  fill: 'currentColor'
+}
 
 type AbstractIcon = {
   children: {
-    attributes: IconAttributes;
-    tag: "path";
-  }[];
-  tag: "svg";
-  attributes: Icon["abstract"][0]["attributes"];
-}[];
+    attributes: IconAttributes
+    tag: 'path'
+  }[]
+  tag: 'svg'
+  attributes: Icon['abstract'][0]['attributes']
+}[]
 
 const props = defineProps({
   show: {
@@ -74,29 +79,9 @@ const props = defineProps({
   id: {
     type: String,
     required: false,
-    default: "loader",
+    default: 'loader',
   },
-  faProps: {
-    type: Object as Vue.PropType<
-      Pick<typeof FontAwesomeIcon.$props, "icon"> &
-        Partial<typeof FontAwesomeIcon.$props>
-    >,
-  },
-  // iconSize: {
-  //   type: String as Vue.PropType<'xs'|'sm'|'lg'|'2x'|'3x'|'5x'|'7x'|'10x'>,
-  //   required: false,
-  //   default: '3x',
-  //   validator: (val : string) => [
-  //       'xs'
-  //       , 'sm'
-  //       , 'lg'
-  //       , '2x'
-  //       , '3x'
-  //       , '5x'
-  //       , '7x'
-  //       , '10x'
-  //     ].includes(val)
-  // },
+  faProps: { default: { size: '3x' }, type: Object as Vue.PropType<Partial<Omit<typeof FontAwesomeIcon.$props, 'icon'|'size'>> & Pick<typeof FontAwesomeIcon.$props, 'size'>> },
   duration: {
     default: 0.25,
     type: Number,
@@ -110,22 +95,22 @@ const props = defineProps({
   ease: {
     type: String,
     required: false,
-    default: "power2.inOut",
+    default: 'power2.inOut',
   },
   endState: {
+    type: Object as Vue.PropType<Partial<gsap.CSSProperties>>,
     default: { opacity: 0 },
   },
-});
-
-const emit = defineEmit({ done: () => true });
-const thisRef = ref();
-const { show } = toRefs(props);
-const spin = ref(show.value);
-const cogAbstract = FA.icon(lightCog).abstract;
-const checkCircleAbstract = FA.icon(lightCheckCircle).abstract;
-const iconRef = ref();
-const fromIcon = getPathData(cogAbstract)?.d;
-const toIcon = getPathData(checkCircleAbstract)?.d;
+})
+const emit = defineEmit({ done: () => true })
+const thisRef = ref()
+const { show } = toRefs(props)
+const spin = ref(show.value)
+const cogAbstract = FA.icon(lightCog).abstract
+const checkCircleAbstract = FA.icon(lightCheckCircle).abstract
+const iconRef = ref()
+const fromIcon = getPathData(cogAbstract)?.d
+const toIcon = getPathData(checkCircleAbstract)?.d
 const tl = gsap.timeline({
   defaults: {
     duration: props.duration,
@@ -134,45 +119,47 @@ const tl = gsap.timeline({
   },
   paused: true,
   id: props.id,
-});
+})
 
 watchEffect(
   () => {
-    const iconEl = iconRef.value.$el;
-    const componentEl = thisRef.value;
+    const iconEl = iconRef.value.$el
+    const componentEl = thisRef.value
+
     // console.log({iconRef: iconRef.value.$el, thisRef: toRaw(thisRef.value)})
-    tl.to(gsap.utils.selector(iconEl)("path"), {
+
+    tl.to(gsap.utils.selector(iconEl)('path'), {
       morphSVG: toIcon,
       id: `${props.id}_morph`,
     }).to(componentEl, {
       delay: 0.25,
       id: `${props.id}_fade`,
       ...props.endState,
-    });
+    })
   },
-  { flush: "post" } // See https://v3.vuejs.org/guide/composition-api-template-refs.html#watching-template-refs for meaning
-);
+  { flush: 'post' } // See https://v3.vuejs.org/guide/composition-api-template-refs.html#watching-template-refs for meaning
+)
 function animateOut(el: Element, done: () => void) {
-  spin.value = false;
-  tl.delay(props.delay).play().then(done);
+  spin.value = false
+  tl.delay(props.delay).play()
+    .then(done)
 }
 function animateIn(el: Element) {
-  spin.value = false;
+  spin.value = false
   tl.delay(props.delay)
     .reverse()
-    .then(() => (spin.value = true));
+    .then(() => (spin.value = true))
 }
 
 function getPathData(arg: Icon | AbstractIcon | FA.AbstractElement[]): {
-  d: gsap.SVGPathValue;
+  d: gsap.SVGPathValue
 } {
-  if (hasPathdata(arg)) return arg[0].children[0].attributes;
-  else return { d: "" };
+  if (hasPathdata(arg)) return arg[0].children[0].attributes
+
+  return { d: '' }
 }
 
-function hasPathdata(
-  arg: Icon | AbstractIcon | FA.AbstractElement[]
-): arg is AbstractIcon {
-  return (arg as AbstractIcon)[0]?.children[0]?.attributes?.d !== undefined;
+function hasPathdata(arg: Icon | AbstractIcon | FA.AbstractElement[]): arg is AbstractIcon {
+  return (arg as AbstractIcon)[0]?.children[0]?.attributes?.d !== undefined
 }
 </script>
