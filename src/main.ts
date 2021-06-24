edfcbea1c9c5247058414152a353abe27555b7dc
bkoplin <17945159+bkoplin@users.noreply.github.com>
2021-06-24 05:56:06 -0500

    add bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,27 @@
-import {createApp} from "vue";
+import { createApp } from 'vue'
 
-import App from "./App.vue";
+import PrimeVue from 'primevue/config'
+import Tooltip from 'primevue/tooltip'
 
-import PrimeVue from "primevue/config";
-import Tooltip from "primevue/tooltip";
+import { store } from '@/store'
+import App from './App.vue'
 
-import {store} from "@/store";
+import 'primeflex/primeflex.css'
+import 'primevue/resources/themes/bootstrap4-light-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@/style/main.css'
 
-import "primeflex/primeflex.css";
-import "primevue/resources/themes/bootstrap4-light-blue/theme.css";
-import "primevue/resources/primevue.min.css";
-import "primeicons/primeicons.css";
-import "@/style/main.css";
+const app = createApp(App)
 
-const app = createApp(App);
+app.use(PrimeVue)
+app.use(store)
+app.directive('tooltip', Tooltip)
 
-app.use(PrimeVue);
-app.use(store);
-app.directive("tooltip", Tooltip);
-
-const vm = app.mount("#app");
+const vm = app.mount('#app')
 
 export default {
-    app,
-    vm,
-};
+  app,
+  vm,
+}
