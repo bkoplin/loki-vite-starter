88ed176e3d8e5d22adef1d4a675705fed9fe4185
bkoplin <17945159+bkoplin@users.noreply.github.com>
2021-07-09 07:02:34 -0500

    add browser location info to home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="w-full h-full">
-    <div class="text-red-500 align-middle text-center">
+    <div class="text-red-500 text-xl font-light align-middle text-center">
       Hello World
     </div>
+    <div class="font-light text-xl text-green-700">BROWSER LOCATION</div>
+    <vue-json-pretty
+      class="w-full m-2"
+      :data="location"
+    ></vue-json-pretty>
   </div>
 </template>
+
+<script setup lang='ts'>
+import { computed, defineEmit, defineProps, reactive, ref, toRef, toRefs, watch, watchEffect } from 'vue'
+import type { Component, EmitsOptions, PropType, Ref } from 'vue'
+import VueJsonPretty from 'vue-json-pretty'
+import { useBrowserLocation } from '@vueuse/core'
+
+const location = useBrowserLocation()
+
+</script>
