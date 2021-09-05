88aab3074303922cae6699d5d5c76f00ed518cfd
bkoplin <17945159+bkoplin@users.noreply.github.com>
2021-09-05 06:31:27 -0500

    add postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,25 @@
 /* eslint-disable global-require */
 /* eslint-disable @typescript-eslint/no-var-requires */
-module.exports = {
-    plugins: [
-      require('postcss-import'),
-      require('tailwindcss'),
-      require('postcss-nested'),
-      require('autoprefixer'),
-    ],
-  }
-  
+const plugins = [
+  require('postcss-import'),
+  require('precss'),
+  require('postcss-sorting'),
+  require('tailwindcss')({ ...require('./tailwind.config') }),
+  require('autoprefixer'),
+  // require('postcss-pxtorem')({ rootValue: 14.8, unitPrecision: 0.1 }),
+  // require('postcss-color-rgba-fallback'),
+  // require('postcss-font-magician')({
+  //   variants: {
+  //     Calibri: {
+  //       300: [],
+  //       400: [],
+  //       500: [],
+  //       600: [],
+  //       700: [],
+  //     },
+  //   },
+  //   foundries: ['google'],
+  // }),
+]
+
+module.exports = { plugins }
