4afbe118faff5b79eea73e45dfc028ca90ecc797
bkoplin <17945159+bkoplin@users.noreply.github.com>
2021-09-05 06:38:16 -0500

    upgrade dependencies

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ import 'primevue/resources/themes/bootstrap4-light-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import '@/style/main.scss'
+import '@/style/main.css'
 import 'vue-json-pretty/lib/styles.css'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
